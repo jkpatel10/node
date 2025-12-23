946da8bf20d4c798d607d71097b6e806f2c1defe
jkpatel10 <jinal210kk@gmail.com>
2025-12-23 15:25:36 +0530

    to-do

---
+++ project/to-do/index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+port = 1008
+
+const app = express()
+
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"));
+
+let task = []
+
+app.get("/",(req,res)=>{
+    res.render("index",{task})
+})
+
+app.post("/addData",(req,res)=>{
+    let data = {
+        id : Date.now(),
+        ...req.body
+    }
+    task.push(data)
+
+    res.redirect("/")
+})
+
+app.get("/deleteData/:id",(req,res)=>{
+    let newData = task.filter((item)=>item.id != req.params.id)
+    task = newData
+    res.redirect("/")
+})
+
+app.get("/editData",(req,res)=>{
+    let singleData = task.find((item)=>item.id == req.query.id)
+    res.render("edit",{singleData})
+})
+
+app.post("/updateData",(req,res)=>{
+    let singleData = task.find((item)=>item.id == req.body.id)
+
+    singleData.task = req.body.task
+    singleData.priority = req.body.priority
+
+    res.redirect("/")
+})
+
+app.listen(port,(err)=>{
+    err ? console.log(err) : console.log(`Server started on port ${port}`);
+})
